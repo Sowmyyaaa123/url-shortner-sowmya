f86af477641d58f46770cc43390b0d8ab7197ab6
Sowmya <mentesowmya7@gmail.com>
2026-05-11 13:37:22 +0530

    initial c

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -53,7 +53,7 @@ const Home = () => {
               marginTop: "1rem",
             }}
           >
-            Your App Name
+            Sowmya's url shortner
           </Title>
 
           <Text
