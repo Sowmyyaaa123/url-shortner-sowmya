b24be6bc60215376f0c2f53f9892170f249e7132
Sowmya <mentesowmya7@gmail.com>
2026-05-15 13:36:24 +0530

    fixed shortenurl route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,40 @@
-import './App.css'
+import './App.css';
+import './index.css';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Home from './Pages/Home/Home';
 import LoginPage from './Pages/LoginPage/LoginPage';
-import './index.css';
+import MyUrls from './Pages/MyUrls/MyUrls';
+
 import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
 import { HeaderMegaMenu } from './Components/Navbar/HeaderMegaMenu';
 import Profile from './Components/Profile/Profile';
-import ShortenUrl from './Pages/ShortenUrl/ShortenUrl';
-import MyUrls from './Pages/MyUrls/MyUrls';
 
-
-
+// UPDATED IMPORT
+import Input from './Pages/ShortenUrl/Input';
 
 function App() {
- return (
-   <Router>
-       <HeaderMegaMenu/>
-       <Routes>
-           <Route path='/' element={<Home/>}/>
-           <Route path='/profile' element={<Profile />} />
-           <Route path='/login' element={<LoginPage/>} />
-           <Route path='/url/shortener' element={<ShortenUrl/>} />
-           <Route element={<PrivateRoute/>}>
-            <Route path='/url/list' element={<MyUrls/>} />
+  return (
+    <Router>
+      <HeaderMegaMenu />
 
+      <Routes>
+        <Route path='/' element={<Home />} />
 
-           </Route>
-       </Routes>
-   </Router>
- )
+        <Route path='/profile' element={<Profile />} />
+
+        <Route path='/login' element={<LoginPage />} />
+
+        {/* UPDATED ROUTE */}
+        <Route path='/url/shortener' element={<Input />} />
+
+        <Route element={<PrivateRoute />}>
+          <Route path='/url/list' element={<MyUrls />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
-
-export default App
+export default App;
